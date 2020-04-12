ff9777d2527163834db68257513147538cb9140b
Andriy Trofimov <webgrandmo@gmail.com>
2020-04-12 15:52:14 +0300

    Add logo and config gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ var sass = require('gulp-sass');
 // -----------------
 
 gulp.task('sass', function() {
-  return gulp.src('./style/scss/style.scss') // Gets all files ending with .scss in app/scss and children dirs
+  return gulp.src('./public/style/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
-    .pipe(gulp.dest('style')); // Outputs it in the css folder
+    .pipe(gulp.dest('./public/style')); // Outputs it in the css folder
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./style/scss/style.scss', gulp.series('sass'));
+  gulp.watch('./public/style/scss/**/*.scss', gulp.series('sass'));
 });
 
 gulp.task('default', gulp.series('watch'), gulp.series('sass'));
