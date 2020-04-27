a9527da22f6b0255f2a88fc4e9a8841d15ce2a3c
Andriy Trofimov <andriy.trofimov@carus.com>
2020-04-27 21:21:08 +0300

    football-highlights/sorting-and-filtering. Implement filtering and search

--- views/assets/js/app.js
+++ views/assets/js/app.js
@@ -1,5 +1,8 @@
 const field = document.querySelector('.search-input'),
-    element = document.querySelector('.match-item');
+    element = document.querySelectorAll('.match-item'),
+    select = document.querySelector('.filter-input'),
+    container = document.getElementById('match-container'),
+    matchItem = document.querySelector('.match-item');
 class SearchInput {
     constructor(searchField, searchElements) {
         this.searchField = searchField;
@@ -7,7 +10,7 @@ class SearchInput {
     }
 
     searchItems() {
-        this.searchField.addEventListener('keyup', (e) => {
+        this.searchField.addEventListener('keypress', (e) => {
             e.preventDefault();
             const query = this.searchField.value.toLowerCase();
             const items = document.querySelectorAll('.match-item');
@@ -30,6 +33,30 @@ class SearchInput {
         });
     }
 }
+class SortInput {
+    constructor(sortField, sortElements, appendContainer) {
+        this.sortField = sortField;
+        this.sortElements = sortElements;
+        this.appendContainer = appendContainer;
+    }
+
+    triggerSortItems() {
+        this.sortField.addEventListener('change', () => {
+            const items = this.sortElements;
+            const selector = document.querySelector('.filter-input');
+            const selectValue = selector.options[selector.selectedIndex].getAttribute('name');
+            const matchItems = Array.from(items).filter(item => {
+                return item.getAttribute('data-name').toLowerCase() === selectValue.toLowerCase()
+            });
+
+            matchItems.forEach((item) => {
+                this.appendContainer.insertBefore(item, this.appendContainer.firstChild);
+            })
+        });
+    };
+}
 
 const searchInput = new SearchInput(field, element);
 searchInput.searchItems();
+const selectInput = new SortInput(select, element, container);
+selectInput.triggerSortItems();
