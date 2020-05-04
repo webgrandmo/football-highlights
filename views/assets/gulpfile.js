afd1bdc863a6154e167b72e04aea9a85ace6e1f2
Andriy Trofimov <andriy.trofimov@carus.com>
2020-05-05 01:32:24 +0300

    fix paths in gulp file

--- views/assets/gulpfile.js
+++ views/assets/gulpfile.js
@@ -1,17 +1,16 @@
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 // Development Tasks 
 // -----------------
 
 gulp.task('sass', function () {
-  return gulp.src('../views/assets/css/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+  return gulp.src('./css/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
-    .pipe(gulp.dest('./views/assets/css')); // Outputs it in the css folder
+    .pipe(gulp.dest('./css')); // Outputs it in the css folder
 });
 
 gulp.task('watch', function () {
-  gulp.watch('../views/assets/css/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('./css/scss/**/*.scss', gulp.series('sass'));
 });
 
 gulp.task('default', gulp.series('watch'), gulp.series('sass'));
